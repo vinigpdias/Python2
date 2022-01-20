000a9c1dbe3db549be8c0f3003adcabc67bbe395
vinigpdias <viniciusgustavo.p.dias@gmail.com>
2022-01-20 17:13:07 -0300

    estudo

---
+++ Ex2.py
@@ -0,0 +1,13 @@
+N1 = int(input("Digite o primeiro número: "))
+N2 = int(input("Digite o segundo número: "))
+
+if N1 > N2:
+    N = N1 - N2
+    print("O primeiro núnmero é maior. Diferença: ", N)
+
+if N2 > N1:
+    N = N2 - N1
+    print("O segundo número é maior. Diferença: ", N)
+
+else:
+    print("Os números são iguais")
