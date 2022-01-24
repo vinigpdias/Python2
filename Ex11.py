95145f55e848ca7cbe975a542155fe31d0a84db2
vinigpdias <viniciusgustavo.p.dias@gmail.com>
2022-01-24 17:30:01 -0300

    estudos - acumuladores

---
+++ Ex11.py
@@ -0,0 +1,17 @@
+#variáreis
+from turtle import clear
+
+
+i = int
+a = int
+b = int
+s = int
+
+a = int(input("digite um número: "))
+b = int(input("Digite um numero para b: "))
+s = 0
+
+for i in range(a,b):
+    s = s + i
+    print(s)
+    #print(i)
---
+++ Ex9.py
@@ -0,0 +1,14 @@
+#variáreis
+i = int
+n = int
+c = int
+s = int
+
+n = int(input("digite um número: "))
+c = int(input("Digite um numero para c: "))
+s = 0
+
+for i in range(1,n):
+    s = s + i
+    print(s)
+    #print(i)
