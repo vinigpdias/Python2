faf925246916e9e4437e96344f6485c4010ae2bf
vinigpdias <viniciusgustavo.p.dias@gmail.com>
2022-01-25 15:11:21 -0300

    estudo de repetição

---
+++ Ex12.py
@@ -0,0 +1,7 @@
+i = int
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i = i + 1
---
+++ Ex13.py
@@ -0,0 +1,10 @@
+from timeit import repeat
+
+i = int
+
+i = 1
+
+repeat
+while i != 10:
+    i = i + 1
+    print(i)
