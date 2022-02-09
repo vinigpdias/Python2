498ab72f2c92d6e422ed444ed088b690db2f69c1
vinigpdias <viniciusgustavo.p.dias@gmail.com>
2022-02-08 21:22:45 -0300

    exercício vetores

--- Ex26.py
+++ Ex26.py
@@ -2,10 +2,18 @@ i = int
 n = int
 c = int
 s = int
+a = int
+
+a = [0,1,2,3,4,5,6,7,8,9,10]
+
 
 n = int(input("digite o total de números: "))
-s = 0
+for i in range(0,n):
+    a[i] = int(input("Insira a Nota: "))
 
-for i in range(1,n):
-    print("Digite o ", i, "º:")
-    print(i)
+s = 0
+for i in range(0,n):
+    s = s + a[i]
+
+    print(s/n) 
+   
